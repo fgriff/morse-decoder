afed35fb2369800af7dfc7ec2e627a4589ec7736
fgriff <anglirum@gmail.com>
2021-07-20 15:11:50 +0300

    feat: implement morse-decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-  
+  let result = '';
+
+  for (let i = 0; i < expr.length; i+=10) {
+    let char = '';
+    const substr = expr.slice(i, i + 10);
+
+    if (substr === '**********') {
+      result += ' ';
+    } else {
+      for (let j = 0; j < substr.length; j+=2) {
+        const subchar = substr.slice(-j - 2, substr.length - j);
+        char += subchar === '11' ? '-' : subchar === '10' ? '.' : '';
+      }
+
+      char = char.split('').reverse().join('');
+      result += MORSE_TABLE[char];
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
